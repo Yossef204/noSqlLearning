6ac4436ee9d4034fae569b277d122f2cba85bbc5
Yossef <ym97989390@gmail.com>
2026-03-06 21:31:44 +0200

    feat:update api

--- src/modules/user/user.controller.js
+++ src/modules/user/user.controller.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { checkUserExist, createUser } from "./user.service.js";
+import { checkUserExist, createUser, filterUserEmailAndPhone, filterUserEmailAndPhoneById, updateUser } from "./user.service.js";
+import { ObjectId } from "mongodb";
 
 const router = Router();
 
@@ -8,24 +9,7 @@ router.post("/", async (req, res, next) => {
     //destructure the body
     const { email, phone } = req.body;
     //check if user exists
-    const userExist = await checkUserExist({
-      $or: [
-        {
-          $and: [
-            { email },
-            { email: { $exists: true } },
-            { email: { $ne: null } },
-          ],
-        },
-        {
-          $and: [
-            { phone },
-            { phone: { $exists: true } },
-            { phone: { $ne: null } },
-          ],
-        },
-      ],
-    });
+    const userExist = await checkUserExist(filterUserEmailAndPhone(email, phone));
     //if exists return error
     if (userExist) {
       throw new Error("User already exists", { cause: 409 });
@@ -44,5 +28,36 @@ router.post("/", async (req, res, next) => {
     });
   }
 });
+
+router.put("/:id", async (req, res, next)=>{
+ try {
+   //destruct the params
+  const {id} = req.params;
+  //destructure the body
+  const {name,phone,email}=req.body;
+  //check if user exists
+  const userExist = filterUserEmailAndPhoneById(email,phone,id);
+
+  if(userExist){
+    throw new Error("User already exists", {cause : 409});
+  }
+  const updatedUser = await updateUser({_id : id},{name,phone},{returnDocument : "after"});
+  //if not exists return error
+  if(!updatedUser){
+    throw new Error("User not found", {cause: 404});
+  }
+  //else update user and return success
+  return res.status(200).json({
+    success: true,
+    message: "User updated successfully",
+    data: {user: updatedUser}
+  });
+ } catch (error) {
+  res.status(error?.cause || 500).json({
+    success: false,
+    message: error.message,
+  });
+ }
+})
 
 export default router;
--- src/modules/user/user.service.js
+++ src/modules/user/user.service.js
@@ -1,11 +1,65 @@
+import { ObjectId } from "mongodb";
 import { User } from "../../DB/index.js";
+import { db } from "../../DB/connection.js";
 
-export const checkUserExist = async (data)=>{
-   const user = await User.findOne(data)   // {} | null
-    return user;
+export const checkUserExist = async (data) => {
+  const user = await db.collection("users").findOne(data); // {} | null
+  return user;
 };
 
+export const createUser = async (data) => {
+  return await db.collection("users").insertOne(data);
+};
 
-export const createUser = async (data)=>{
-    return await User.insertOne(data);
+export const updateUser = async (filter, data, options) => {
+  filter._id = new ObjectId(filter._id);
+  return await db
+    .collection("users")
+    .findOneAndUpdate(filter, { $set: data }, options);
+};
+
+export const filterUserEmailAndPhone = (email, phone) => {
+  return {
+    $or: [
+      {
+        $and: [
+          { email: { $exists: true } },
+          { email: { $ne: null } },
+          { email },
+        //   { _id: { $ne: new ObjectId(id) } },
+        ],
+      },
+      {
+        $and: [
+          { phone: { $exists: true } },
+          { phone: { $ne: null } },
+          { phone },
+        //   { _id: { $ne: new ObjectId(id) } },
+        ],
+      },
+    ],
+  };
+};
+
+export const filterUserEmailAndPhoneById = (email, phone,id) => {
+  return {
+    $or: [
+      {
+        $and: [
+          { email: { $exists: true } },
+          { email: { $ne: null } },
+          { email },
+          { _id: { $ne: new ObjectId(id) } },
+        ],
+      },
+      {
+        $and: [
+          { phone: { $exists: true } },
+          { phone: { $ne: null } },
+          { phone },
+          { _id: { $ne: new ObjectId(id) } },
+        ],
+      },
+    ],
+  };
 };
